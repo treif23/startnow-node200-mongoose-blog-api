63dbacfb0c76548e8a57d947e856989dfea6001a
Tommy <treif23@github.com>
2018-05-30 10:58:02 -0700

    update 5/30/18

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-blog", { useMongoClient: true });
 
 mongoose.Promise = Promise;
 
